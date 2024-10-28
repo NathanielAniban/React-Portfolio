2fd7e2a489ea183140c0d182393fdb47dbace1d9
NathanielAniban <114843841+NathanielAniban@users.noreply.github.com>
2024-10-28 08:15:47 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
-import { useState } from 'react'
-import { Container, Button } from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
+import Home from './Pages/Home'
 
 function App() {
-  const [submit, submitted] = useState('primary');
-  const Click = (event) => {
-    const target =  document.getElementById(event.target.id);
-    if(true){
-      submitted('success');
-      target.textContent = "Submitted";
-      target.disabled = true;
-      target.setAttribute('variant', 'primary');
-    }
-  };
-
-  return (
-    <>
-     <Container fluid className='mt-3'>
-        <Button variant={submit} id='button' onClick={Click}>Submit</Button>
-     </Container>
-    </>
-  )
+ return(
+  <>
+    <Home/>
+  </>
+ );
 }
 
 export default App
